b2b6d58a2630f3581dc3952c28a9d8eae7b67953
Hekel1989 <>
2024-09-30 21:46:46 +0100

    "Removed expect tool installation and usage, replaced with 'yes y' pipe to automate script prompts"

--- scripts/cachyos_repos.sh
+++ scripts/cachyos_repos.sh
@@ -4,24 +4,12 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 
 echo "Setting up CachyOS repos..."
 
-# Install necessary tools
-sudo pacman -S --noconfirm expect
-
 curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz
 cd cachyos-repo
 
-# Use expect to automate the response
-expect << EOF
-spawn sudo ./cachyos-repo.sh
-expect {
-    "proceed with installation? \\\[y/n\\\]" {
-        send "y\r"
-        exp_continue
-    }
-    eof
-}
-EOF
+# Run the script with automatic "yes" to all prompts
+yes y | sudo ./cachyos-repo.sh
 
 cd ..
 rm -rf cachyos-repo cachyos-repo.tar.xz
