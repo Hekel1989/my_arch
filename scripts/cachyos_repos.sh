eb657f4912ed50bc9104d094aefe116cb86afa03
Hekel1989 <>
2024-09-30 21:37:43 +0100

    updated cachyos_repos.sh

--- scripts/cachyos_repos.sh
+++ scripts/cachyos_repos.sh
@@ -4,7 +4,7 @@ echo "Setting up CachyOS repos..."
 curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz
 cd cachyos-repo
-sudo ./cachyos-repo.sh
+sudo ./cachyos-repo.sh --no-confirm
 cd ..
 rm -rf cachyos-repo cachyos-repo.tar.xz
 echo "CachyOS repos setup completed."
